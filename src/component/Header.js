908e48d0c55d57e9c0bd1902c75ec0d234de013d
Mahmoud Awad <awad@talaween.net>
2018-06-27 12:01:49 +0100

    added responsive search input to the header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './Header.css';
-import react from '../img/react.svg';
+import react_logo from '../img/react_logo.svg';
 
 class Header extends Component {
   render() {
 
+    var FontAwesome = require('react-fontawesome');
+
     return (
 
         <div class="header">
-            <img src={react} alt="React logo" /><a href="#default" class="logo"> React App to cosume Flicker API</a>
+            <img src={react_logo} alt="React logo" /><a href="#default" class="logo"> React App to cosume Flicker API</a>
             <div class="header-right">
-                <a class="active" href="http://awad.talaween.net">About Developer</a>
+                <div class="search-container">
+                    <form action="">
+                        <input type="text" placeholder="Search.." name="search" />
+                        <button type="submit"><FontAwesome name='search' /></button>
+                    </form>
+                </div>
             </div>
         </div>
        
